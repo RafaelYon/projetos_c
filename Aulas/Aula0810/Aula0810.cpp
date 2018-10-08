96b39473960e5d0bd573cb584363163d3cf74011
Rafael Antonio Yon <rafaelyon.1999@gmail.com>
2018-10-08 09:41:27 -0300

    Incluindo bibliotecas, instanciando variáveis.

--- Aulas/Aula0810/Aula0810.cpp
+++ Aulas/Aula0810/Aula0810.cpp
@@ -1,11 +1,25 @@
 // Aula0810.cpp : Defines the entry point for the console application.
-//
 
-#include "stdafx.h"
+#include "stdafx.h" // Inclusão de biblioteca
 
+// C ANSI
+#include <stdio.h>	// Biblioteca de entrada e saída padrão
+#include <stdlib.h>	// Biblioteca para dar comandos no CMD
+
+// Variaveis (Escopo Global - visível em todo o código).
+// Uma variável só exite após sua criação.
+int inteiro;
+float ponto_flutuante;	// Precissão menor
+double real;			// Precissão maior
+
+// Bool -> Implementado nativamente no C - C99. Implementado pela biblioteca stdbool.h no C89/90. (https://stackoverflow.com/questions/1608318/is-bool-a-native-c-type)
+bool logico;
 
 int main()
 {
-    return 0;
+	// Variavel (Escopo Local - visível apenas nesta função).
+	int local_inteiro;
+	
+	
+	return 0;
 }
-
