a64c63469509cc1a73652188dd38c6288eecac09
Rafael Antonio Yon <rafaelyon.1999@gmail.com>
2018-10-10 10:25:41 -0300

    Adicionado funções básicas

--- Aulas/Aula1010_funcoes/Aula1010_funcoes.cpp
+++ Aulas/Aula1010_funcoes/Aula1010_funcoes.cpp
@@ -3,9 +3,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// void -> retorno "vazio" (não retorna nada)
+void HelloWorld()
+{
+	printf("Hello World!\n");
+}
+
+int Soma2Valores(int a, int b)
+{
+	return a + b;
+}
+
+int LerInt()
+{
+	int value;
+
+	printf("Insira um numero inteiro: ");
+
+	scanf_s("%i", &value);
+
+	return value;
+}
+
 int main()
 {
-	
+	// Chama função sem retorno
+	HelloWorld();
+
+	// Exibe o resultado de uma função que recebe como parâmetros os resultados de outra função.
+	printf("Soma: %i", Soma2Valores(LerInt(), LerInt()));
+
+	system("pause");
 	
 	return 0;
 }
