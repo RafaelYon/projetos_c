0e3ec4e38fdef5c9591a976b662e08a3e7a604e5
Rafael Antonio Yon <rafaelyon.1999@gmail.com>
2018-10-22 10:47:11 -0300

    Adicionado funções para calcular média e para exibir os valores que são maiores que o valor de sua média.

--- Aulas/Aula2210_Condicionais/Aula2210_Condicionais.cpp
+++ Aulas/Aula2210_Condicionais/Aula2210_Condicionais.cpp
@@ -36,12 +36,48 @@ int get_largest_int(int a, int b, int c, int d)
 	return largest;
 }
 
+float get_avarage(float a, float b, float c, float d)
+{
+	return (a + b + c + d) / 4;
+}
+
+void print_int_largest_than_avarage(int largest, float avarage)
+{
+	printf("O valor %d e maior que a media de %f.\n", largest, avarage);
+}
+
+
+void show_largest_than_avarage(int a, int b, int c, int d)
+{
+	float avarage = get_avarage(a, b, c, d);
+
+	if (a > avarage)
+	{
+		print_int_largest_than_avarage(a, avarage);
+	}
+
+	if (b > avarage)
+	{
+		print_int_largest_than_avarage(b, avarage);
+	}
+
+	if (c > avarage)
+	{
+		print_int_largest_than_avarage(c, avarage);
+	}
+
+	if (d > avarage)
+	{
+		print_int_largest_than_avarage(d, avarage);
+	}
+}
+
 
 int main()
 {
 	int a, b, c, d, result;
 	
-	printf("Descubra qual dos 4 inteiros e o maior:\n");
+	printf("Descubra qual dos 4 inteiros e o maior e os valores que são maiores que a media dos mesmos:\n");
 
 	a = read_int();
 	b = read_int();
@@ -51,6 +87,8 @@ int main()
 	result = get_largest_int(a, b, c, d);
 
 	printf("O maior entre os 4 valores informados e: %d\n", result);
+
+	show_largest_than_avarage(a, b, c, d);
 	
 	system("pause");
 	
