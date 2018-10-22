b1178adad68beb6f9db9cc2adf8b2fba9a1107b5
Aluno <aluno>
2018-10-22 08:51:59 -0300

    Criado programa que lê 4 inteiros e exibe o maior entre eles

--- Aulas/Aula2210_Condicionais/Aula2210_Condicionais.cpp
+++ Aulas/Aula2210_Condicionais/Aula2210_Condicionais.cpp
@@ -1,11 +1,56 @@
-// Aula2210_Condicionais.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int read_int()
+{
+	int result;
+	
+	printf("Insira um numero inteiro: ");
+
+	scanf_s("%d", &result);
+
+	return result;
+}
+
+int get_largest_int(int a, int b, int c, int d)
+{
+	if (a > b && a > c && a > d)
+	{
+		return a;
+	}
+	else if (b > a && b > c && b > d)
+	{
+		return b;
+	}
+	else if (c > a && c > b && c > d)
+	{
+		return c;
+	}
+	else
+	{
+		return d;
+	}
+}
 
 
 int main()
 {
-    return 0;
-}
+	int a, b, c, d, result;
+	
+	printf("Descubra qual dos 4 inteiros e o maior:\n");
 
+	a = read_int();
+	b = read_int();
+	c = read_int();
+	d = read_int();
+
+	result = get_largest_int(a, b, c, d);
+
+	printf("O maior entre os 4 valores informados e: %d\n", result);
+	
+	system("pause");
+	
+	return 0;
+}
