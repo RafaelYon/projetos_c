78b7003283e094ae7c48a793f96e6fcd733bd06a
Rafael Antonio Yon <rafaelyon.1999@gmail.com>
2018-10-29 10:32:21 -0300

    Adicionado função que lê 5 numeros e diz se os mesmo estão em ordem crescentes. Esta função foi criada para seguir os desafios: * Escrever a função que lê a entrada de dados apenas uma vez; * Usar menos de 5 variaveis;

--- Aulas/Aula2910/Aula2910.cpp
+++ Aulas/Aula2910/Aula2910.cpp
@@ -1,5 +1,4 @@
 #include "stdafx.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 int read_int()
@@ -13,16 +12,16 @@ int read_int()
 	return value;
 }
 
-
-int main()
+void show_numbers_between_ints()
 {
 	int a, b;
 
 	a = read_int();
 	b = read_int();
-	
+
 	printf("Exibe valores entres os dois valores digitados utilizando o loop \"while\":\n");
 
+	// Verifica se precissa trocar a ordem dos valores.
 	if (a > b)
 	{
 		int temp = a;
@@ -33,11 +32,48 @@ int main()
 	}
 
 	while (a < (b - 1))
-	{	
+	{
 		a++;
 
 		printf("%d\n", a);
 	}
+}
+
+// Função que pede 5 valores e exibe se estão em ordem crescente
+void ask_for_five_ints_show_asc_or_desc()
+{
+	bool asc = true;
+	int currentValue, previousValue;
+
+	for (int i = 0; i < 5; i++)
+	{
+		currentValue = read_int();
+
+		// Não realiza a verificação na primeira execução.
+		if (asc == true && i > 0 && currentValue < previousValue)
+		{
+			asc = false;
+		}
+
+		previousValue = currentValue;
+	}
+
+	if (asc)
+	{
+		printf("Os numeros estao em ordem crescente.\n");
+	}
+	else
+	{
+		printf("Os numeros nao estao em ordem crescente.\n");
+	}
+}
+
+
+int main()
+{
+	//show_numbers_between_ints();
+
+	ask_for_five_ints_show_asc_or_desc();
 
 	system("pause");
 
